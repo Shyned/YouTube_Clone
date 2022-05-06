367cad810f04a4c6c16cc708317f683419a10a1e
Dyshine Hill <eyedolarts@gmail.com>
2022-05-06 11:40:26 -0500

    made some style changes + added video to video page

--- youtube-clone-frontend/src/components/VideoPages/VidepPage.jsx
+++ youtube-clone-frontend/src/components/VideoPages/VidepPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const VideoPage = props => {
-  return <div>hey</div>;
+  return(
+  <iframe id="ytplayer" type="text/html" width="640" height="360"
+  src="https://www.youtube.com/embed/Y8Tko2YC5hA"
+  frameborder="0"></iframe>
+  )
 };
 
 export default VideoPage;
--- youtube-clone-frontend/src/pages/searchpage/searchpage.jsx
+++ youtube-clone-frontend/src/pages/searchpage/searchpage.jsx
@@ -9,19 +9,28 @@ import "./searchpage.css"
 const SearchPage = () => {
     const [search, setSearch] = useState([]);
     const [searchResults, setSearchResults] = useState([])
+    const [currentVideo,setCurrentVideo ] = useState ([])
 
 
     useEffect(()=>{
       getSearchResults()
     }, [search])
+// get data from clicked card
+function getVideo(vid){
+  let tempvideo = vid
+  console.log(tempvideo)
+  setCurrentVideo(tempvideo)
+
+}
+
+
 
   function findItem(keyword){
     let tempkey = keyword
-    console.log(tempkey)
     setSearch(tempkey)
   }
 
-    async function getSearchResults(searchTerm = "bob ross"){
+    async function getSearchResults(){
       let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${search}&key=AIzaSyDVGusdch7Zf0faFb7cYBfEibEc1aM3PIs`)
       console.log(response.data.items)
       setSearchResults(response.data.items)
@@ -35,11 +44,12 @@ const SearchPage = () => {
         <div className='video-section'></div>
         {searchResults.map((video,index)=>{
           return(
-            
-            <div className='video-card'>
-            <img src = {video.snippet.thumbnails.medium.url} alt='thumbnail'/>
+            // on click set current video to click video data to be passed to app.js to  video page
+            // need to return video id 
+            <button onClick={null} className='video-card' key = {index}>
+            <img className='search-thumbnail' src = {video.snippet.thumbnails.high.url} alt='thumbnail'/>
               <p>{video.snippet.title}</p>
-            </div>
+            </button>
           )
         })}
 
